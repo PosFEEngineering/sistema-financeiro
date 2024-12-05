2830c18d1fb7141e5782f52f68847c7c68ab68e6
Amanda Vieira <amandavr@gmail.com>
2024-12-04 22:51:51 -0300

    feat: install storybook - wip

---
+++ .storybook/main.ts
@@ -0,0 +1,54 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-designs",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  core: {
+    builder: {
+      name: "@storybook/builder-webpack5",
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+  webpackFinal: async (config) => {
+    // Garante que arquivos CSS sejam processados corretamente
+    config.module?.rules?.push({
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          },
+        },
+        'postcss-loader',
+      ],
+      include: [require('path').resolve(__dirname, '../')],
+    });
+
+    return config;
+  },
+};
+
+export default config;
